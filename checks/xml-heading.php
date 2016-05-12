9b40c55f4b815d76ecadbb4150cf3f2487c0d16c
Keanan Koppenhaver <keanan@doejo.com>
2016-05-12 16:42:58 -0700

    Initial check: Make sure the xml tag is present at the top of the file

--- checks/xml-heading.php
+++ checks/xml-heading.php
@@ -1,4 +1,20 @@
 <?php
 
-	$xml_file = fopen( 'test.xml', 'r' ) or die( 'Unable to open file!' );
+$status = 'not_passed';
+$xml_file = fopen( 'test.xml', 'r' ) or array_push( $errors, 'Could not find XML file.' );
+
+while ( $line = fgets( $xml_file ) ) {
+	preg_match( '<\?xml version=".*?" encoding=".*?"\?>', $line, $matches );
+	if ( count( $matches ) > 0 ) {
+		$status = 'passed';
+		break;
+	}
+}
+
+if ( 'passed' != $status ) {
+	array_push( $errors, 'ERROR: XML Tag Not Found' );
+}
+
+fclose( $xml_file );
+
 
--- wxr-validator.php
+++ wxr-validator.php
@@ -14,3 +14,12 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 $errors = array();
 
 include 'checks/xml-heading.php';
+
+if ( empty( $errors ) ) {
+	echo 'You are all set! No errors found.';
+}
+else {
+	foreach ( $errors as $error ) {
+		echo $error . PHP_EOL;
+	}
+}
