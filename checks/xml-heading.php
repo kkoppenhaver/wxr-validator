83db9facf73b32fede103d95999ce1ac99375cfa
Keanan Koppenhaver <keanan@doejo.com>
2016-05-12 16:24:22 -0700

    Init commit

---
+++ checks/xml-heading.php
@@ -0,0 +1,4 @@
+<?php
+
+	$xml_file = fopen( 'test.xml', 'r' ) or die( 'Unable to open file!' );
+
---
+++ wxr-validator.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: WXR Validator
+Plugin URI: https://github.com/kkoppenhaver/wxr-validator
+Description: This script will run checks on any WXR (WordPress eXtended RSS) file to ensure the syntax is valid before a WordPress import is run.
+Author: kkoppenhaver
+Author URI: http://keanankoppenhaver.com
+Version: 0.1
+Text Domain: wxr-validator
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+$errors = array();
+
+include 'checks/xml-heading.php';
